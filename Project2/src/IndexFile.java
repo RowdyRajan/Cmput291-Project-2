959c73abdff341b105f5c4bff96d161264d51d31
RowdyRajan <rajanram@hotmail.com>
2015-04-10 13:56:20 -0600

    Added to index file

--- Project2/src/IndexFile.java
+++ Project2/src/IndexFile.java
@@ -1,10 +1,95 @@
+import java.util.Random;
+
+import com.sleepycat.db.Database;
+import com.sleepycat.db.DatabaseConfig;
+import com.sleepycat.db.DatabaseEntry;
+import com.sleepycat.db.DatabaseException;
+import com.sleepycat.db.DatabaseType;
+
 
 public class IndexFile implements DataBaseType {
 
+	Database database;
+	//File name for the table
+	private static final String MY_DB_TABLE = "/tmp/egsmith_db/myTable";
+	private static final int NUM_RECORDS = 1000;
+	
 	@Override
 	public void populate() {
-		// TODO Auto-generated method stub
+
+		int range;
+		DatabaseEntry kdbt, ddbt;
+		String s;
 		
+		try {
+			DatabaseConfig dbConfig = new DatabaseConfig();
+		    dbConfig.setType(DatabaseType.HASH);
+		    dbConfig.setAllowCreate(true);
+		    database = new Database(MY_DB_TABLE, null, dbConfig);
+ 
+		    System.out.println(MY_DB_TABLE + " successfully created!");
+			
+		    /*  Generate a random string with the length between 64 and 127,
+			 *  inclusive.
+			 *
+			 *  Seeded once only.
+			 *  
+			 *  Population maintains nearly the same code as the sample population code
+			 */
+			Random random = new Random(1000000);
+
+		        try {
+		            for (int i = 0; i < NUM_RECORDS; i++) {
+		
+						/* to generate a key string */
+						range = 64 + random.nextInt( 64 );
+						s = "";
+						for ( int j = 0; j < range; j++ ) {
+						  s+=(new Character((char)(97+random.nextInt(26)))).toString();
+						}
+						System.out.println(s);	
+			            System.out.println("");
+						
+			
+						/* to create a DBT for key */
+						kdbt = new DatabaseEntry(s.getBytes());
+						kdbt.setSize(s.length()); 
+			
+				        // to print out the key/data pair
+				        // System.out.println(s);	
+			
+						/* to generate a data string */
+						range = 64 + random.nextInt( 64 );
+						s = "";
+						for ( int j = 0; j < range; j++ ) { 
+							s+=(new Character((char)(97+random.nextInt(26)))).toString();
+					        // to print out the key/data pair
+			                // System.out.println(s);	
+			                // System.out.println("");
+						}
+						System.out.println(s);	
+			            System.out.println("");
+						
+						
+						/* to create a DBT for data */
+						ddbt = new DatabaseEntry(s.getBytes());
+						ddbt.setSize(s.length()); 
+			
+						/* to insert the key/data pair into the database */
+			            database.putNoOverwrite(null, kdbt, ddbt);
+		            }
+		            System.out.println(MY_DB_TABLE + " Populated!");
+		        }
+		        catch (DatabaseException dbe) {
+		            System.err.println("Populate the table: "+dbe.toString());
+		            System.exit(1);
+		        }		
+
+		}
+		catch (Exception e1) {
+			System.err.println("Create Database Failed" + e1.toString());
+			System.exit(1);
+		}
 	}
 
 	@Override
