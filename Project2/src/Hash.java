3a70ad3745c8f7541160eaa04e25ec0dbdf8f191
smithe0 <egsmith@ualberta.ca>
2015-04-10 10:52:19 -0600

    Proper Table File

--- Project2/src/Hash.java
+++ Project2/src/Hash.java
@@ -14,7 +14,7 @@ public class Hash implements DataBaseType {
 
 	Database database;
 	//File name for the table
-	private static final String MY_DB_TABLE = "/tmp/egsmith_db";
+	private static final String MY_DB_TABLE = "/tmp/egsmith_db/myTable";
 	private static final int NUM_RECORDS = 1000;
 	
 	@Override
