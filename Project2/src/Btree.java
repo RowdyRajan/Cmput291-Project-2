8df055fa286d9d8d7c2e1c58b6bff37687ab979a
RowdyRajan <rajanram@hotmail.com>
2015-04-09 19:41:29 -0600

    Added retrieveByKey

--- Project2/src/Btree.java
+++ Project2/src/Btree.java
@@ -1,4 +1,12 @@
+import java.awt.print.Printable;
+import java.io.FileWriter;
+import java.util.Scanner;
+
 import com.sleepycat.db.Database;
+import com.sleepycat.db.DatabaseEntry;
+import com.sleepycat.db.DatabaseException;
+import com.sleepycat.db.LockMode;
+import com.sleepycat.db.OperationStatus;
 
 
 public class Btree implements DataBaseType {
@@ -15,13 +23,34 @@ public class Btree implements DataBaseType {
 	public void retrieveByKey() {
 		if(database == null){
 			System.out.println("Please populate the database before continung");
+			return;
+		}
+		System.out.println("Please enter the key you are searching for");
+		Scanner scanner = new Scanner(System.in);
+		String keyString = scanner.nextLine();
+		
+		OperationStatus oprStatus;
+		DatabaseEntry key = new DatabaseEntry();
+		DatabaseEntry data = new DatabaseEntry();
+		key.setData(keyString.getBytes());
+		
+		try {
+			oprStatus = database.get(null, key,data, LockMode.DEFAULT);
+		} catch (DatabaseException e) {
+			System.out.println("Error");
+			return;
 		}
 		
+		if (oprStatus == OperationStatus.NOTFOUND){
+			System.out.println("Key not found");
+		}
+		
+		String getData = new String(data.getData());
+		System.out.println(getData);
 	}
 
 	@Override
 	public void retrieveByData() {
-		// TODO Auto-generated method stub
 		
 	}
 
